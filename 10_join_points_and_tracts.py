873fba8eec6ce6f115dae7cd203f4bf894272f4a
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2020-02-02 18:37:50 +0000

    Cleaning up code

--- 10_join_points_and_tracts.py
+++ 10_join_points_and_tracts.py
@@ -1,3 +1,12 @@
+# TODO find a way to bring sample_rvps (as well as other utility functions)
+# into a common library of some sort
+
+# Import utilities for getting GEOIDs and ids of all Census Tracts
+from spatial_diversity_utils import get_all_tract_geoids
+from sum_driving_durations import sum_of_driving_durations_to_knn
+from distance_matrix import read_duration_matrix_from_file
+import human_compactness_utils
+import sample_rvps
 import geopandas as gpd
 import sys
 import numpy as np
@@ -6,107 +15,105 @@ import json
 # TODO try and make this a relative path
 sys.path.append('/home/lieu/dev/geographically_sensitive_dislocation/10_code')
 
-# TODO find a way to bring sample_rvps (as well as other utility functions)
-# into a common library of some sort
-
-# downsamples RVPs from Nick's code
-import sample_rvps
-
-# read and return a 2D array of floats
-from distance_matrix import read_duration_matrix_from_file
-
-# sum_of_driving_durations_to_knn(voter, dds, k: int)
-# voter is a row of the table, dds is a numpy 2D array where dds[idx] gives the distance
-# from a voter idx to all others
-from sum_driving_durations import sum_of_driving_durations_to_knn
-
-# Import utilities for getting GEOIDs and ids of all Census Tracts
-from spatial_diversity_utils import get_all_tract_geoids
-
-
-# Get dictionary of tracts {id: {GEOID: geoid, pop: None, pfs: None}}
-# GEOID to ID mapping
-tract_dict, geoid_to_id_mapping = get_all_tract_geoids()
 
 def get_vrps_within_tracts():
     pass
 
 
-#TODO make it not only read the state of Georgia
+def read_and_process_vrp_shapefile():
+    '''
+        Returns points_downsampled
+    '''
+    # TODO make it not only read the state of Georgia
 
-GEOG_WD = "/home/lieu/dev/geographically_sensitive_dislocation/00_source_data/"
+    GEOG_WD = "/home/lieu/dev/geographically_sensitive_dislocation/00_source_data/"
 
-CDB = gpd.read_file(GEOG_WD +
-"10_US_Congressional_districts/nhgis0190_shapefile_tl2014_us_cd114th_2014/US_cd114th_2014_wgs84.shp")
+    CDB = gpd.read_file(GEOG_WD +
+                        "10_US_Congressional_districts/nhgis0190_shapefile_tl2014_us_cd114th_2014/US_cd114th_2014_wgs84.shp")
 
-STATE_CODE = 13
-DEM_RVPS = f'{GEOG_WD}00_representative_voter_points/points_D_13_2_10000_run1.shp'
-REP_RVPS = f'{GEOG_WD}00_representative_voter_points/points_R_13_2_10000_run1.shp'
-SAMPLE_SIZE = 14000
+    STATE_CODE = 13
+    DEM_RVPS = f'{GEOG_WD}00_representative_voter_points/points_D_13_2_10000_run1.shp'
+    REP_RVPS = f'{GEOG_WD}00_representative_voter_points/points_R_13_2_10000_run1.shp'
+    SAMPLE_SIZE = 14000
 
-DM_PATH = '/home/lieu/dev/geographically_sensitive_dislocation/20_intermediate_files/duration_matrix_georgia_13.dmx'
+    DM_PATH = '/home/lieu/dev/geographically_sensitive_dislocation/20_intermediate_files/duration_matrix_georgia_13.dmx'
 
-# after we get the points, downsample
+    # after we get the points, downsample
 
-points_downsampled = sample_rvps.sample_rvps(CDB, STATE_CODE, DEM_RVPS,
-                                             REP_RVPS, SAMPLE_SIZE)
+    points_downsampled = sample_rvps.sample_rvps(CDB, STATE_CODE, DEM_RVPS,
+                                                 REP_RVPS, SAMPLE_SIZE)
 
-# Convert to WGS84
-points_downsampled = points_downsampled.to_crs({'init': 'epsg:4326'})
-points_downsampled = points_downsampled.drop('index_right', axis=1)
+    # Convert to WGS84
+    points_downsampled = points_downsampled.to_crs({'init': 'epsg:4326'})
 
-# We only need the points' geometry for our purpose
-points_downsampled = points_downsampled[['geometry', 'party']]
-print(len(points_downsampled))
+    # the points have already been spatial joined previously; drop this so we can do further
+    # spatial joins
+    points_downsampled = points_downsampled.drop('index_right', axis=1)
 
-CENSUS_TRACTS = gpd.read_file("/home/lieu/dev/human_compactness/Data_2000/Shapefiles/Tract2000_13.shp")
+    return points_downsampled
 
-CENSUS_TRACTS = CENSUS_TRACTS.to_crs({'init': 'epsg:4326'})
-#print(CENSUS_TRACTS[:10])
-
-# Spatial join all points that lie in a Census Tract
-points_mapped = gpd.sjoin(points_downsampled, CENSUS_TRACTS, how='inner', op='within')
-
-# The important ones are GEOID, geometry and index_right, possibly party
-print(points_mapped[:10])
-#print(list(points_mapped))
-#print(points_mapped[['GEOID', 'geometry']])
-
-point_to_tract_mapping = {}
 
 def form_point_to_tract_mapping(voter, mapping):
     # get point ID
     mapping[voter.name] = geoid_to_id_mapping[voter['GEOID']]
 
-points_mapped.apply(form_point_to_tract_mapping, axis=1, args=[point_to_tract_mapping])
+
+'''
+    INPUT DATA REQUIRED: 
+       
+    1. VRP shapefile
+    2. 2000 Census Tract shapefile
+    3. tract_dict
+    4. GEOID to tract ID mapping (Daryl's initial assignment)
+
+    DESIRED OUTPUT DATA:
+
+    A dictionary of 
+
+    
+
+    PROCEDURE
+
+    1. Read in Nick's VRP shapefile
+    2. Downsample the VRPs (this is deterministic as I always choose seed=0)
+    3. Read in the 2000 Census Tract data
+    4. 
+'''
+
+CENSUS_TRACTS = gpd.read_file(
+    "/home/lieu/dev/human_compactness/Data_2000/Shapefiles/Tract2000_13.shp")
+
+
+# Read in Nick's VRP shapefile and downsample
+
+points_downsampled = read_and_process_vrp_shapefile()
+
+# Get dictionary of tracts {id: {GEOID: geoid, pop: None, pfs: None}} and the
+# GEOID to ID mapping
+
+tract_dict, geoid_to_id_mapping = get_all_tract_geoids()
+
+# Reproject 2000 Census Tracts to use the same projection as downsampled tracts
+CENSUS_TRACTS = CENSUS_TRACTS.to_crs({'init': 'epsg:4326'})
+
+# Spatial join all points that lie in a Census Tract
+# This gives us a GeoDataFrame of each VRP mapped to a GEOID of the tract
+points_mapped = gpd.sjoin(
+    points_downsampled, CENSUS_TRACTS, how='inner', op='within')
+
+# The important ones are GEOID, geometry and index_right, possibly party
+print(points_mapped[:10])
+
+POINT_TO_TRACT_MAPPING = {}
+
+# Populate the point_to_tract_mapping dictionary
+points_mapped.apply(form_point_to_tract_mapping, axis=1,
+                    args=[POINT_TO_TRACT_MAPPING])
 
 # We now have a mapping from VRPs to GEOID, and from GEOID to id (geoid_to_id_mapping).
-
-#print(point_to_tract_mapping)
-print(len(point_to_tract_mapping))
-
-convert_point_distances_to_tract_distances(point_to_tract_mapping, DM_PATH, 'GEORGIA_13_TRACT_DISTANCES.json')
-
-# We can't do this --- Python runs out of memory
-# print("Reading duration matrix from file...")
-# dm = read_duration_matrix_from_file(DM_PATH)
-# Get the 1000 nearest voters
-# print("Getting nearest-1000-voters...")
-# points_mapped['knn_dd_sum'] = points_mapped.apply(sum_driving_durations_to_knn, args = (dm, 1000))
-# print(points_mapped[['knn_dd_sum']])
-
-'''
-pairwise = np.zeros((2000, 2000))
-fh = open(file_name, 'rt')
-line = fh.readline()
-i = 0
-while line:
-    dist = [float(x) for x in line.split('  ')]
-    for j in range(len(dist)):
-    	pairwise[tract[i]][tract[j]] += dist[j]
-    i+=1
-    line = fh.readline()
-'''
+# We can now convert point_distances to tract_distances
+convert_point_distances_to_tract_distances(
+    POINT_TO_TRACT_MAPPING, DM_PATH, 'GEORGIA_13_TRACT_DISTANCES.json')
 
 # Return a dictionary where key is GEOID (of tract) and values are the points
 # in the GEOID
@@ -114,5 +121,3 @@ while line:
 tracts_mapped = points_mapped.groupby(['GEOID'])['geometry'].apply(list)
 
 print(tracts_mapped)
-
-
--- spatial_diversity_utils.py
+++ spatial_diversity_utils.py
@@ -9,7 +9,8 @@ TRACT_SPATIAL_DIVERSITY_SCORES = '/home/lieu/dev/human_compactness/tract_spatial
 STARTING_PLAN_FILE = '/home/lieu/dev/human_compactness/Data_2000/Dual_Graphs/Tract2000_13.json'
 
 
-SPATIAL_DIVERSITY_FACTOR_WEIGHTS = (0.1464, 0.1182, 0.101, 0.0775, 0.0501, 0.0399, 0.0389, 0.0366)
+SPATIAL_DIVERSITY_FACTOR_WEIGHTS = (
+    0.1464, 0.1182, 0.101, 0.0775, 0.0501, 0.0399, 0.0389, 0.0366)
 
 
 # TODO allow specifiying which state
@@ -24,77 +25,24 @@ def get_all_tract_geoids():
 
     with open(STARTING_PLAN_FILE) as f:
         data = json.load(f)
-        
+
         for node in data["nodes"]:
             #tract_dict[node["id"]] = {'geoid': node["GEOID10"], 'pop': None, 'pfs': None}
             #geoid_to_id_mapping[node["GEOID10"]] = node["id"]
 
             # Mapping for 2000 Census Tracts
-            tract_dict[node["id"]] = {'geoid': node["GEOID"], 'pop': None, 'pfs': None}
+            tract_dict[node["id"]] = {
+                'geoid': node["GEOID"], 'pop': None, 'pfs': None}
             geoid_to_id_mapping[node["GEOID"]] = node["id"]
 
-
-    #print(tract_dict)
+    # print(tract_dict)
     return (tract_dict, geoid_to_id_mapping)
 
-def convert_point_distances_to_tract_distances(pttm, DM_PATH, SAVE_FILE_TO):
-    '''Returns a JSON file giving driving durations from all points in the Census Tract
-    to all other points in every other Census Tract.
-    Takes in three things:
-
-        1. A mapping from points to tracts (Dict[PointId : TractId]
-        2. The distance matrix file path
-        3. The path to save the output tract distance file to
-
-    Reads line by line
-    Returns a JSON file with the following schema:
-    {
-        tractid: {tractid: distance, ... },
-        tractid: {tractid: distance, ...},
-        ...
-    }
-    Each entry in the dictionary is the sum of driving durations from each point in
-    the tract to each other point in the tract.
-
-    Note that the duration from tract_id to tract_id can (in fact is almost always nonzero)
-    because it measures the sum of durations from all points in the tract
-    '''
-    tract_distances = {}
-
-    with open(DM_PATH, 'rt') as fh:
-        line = fh.readline()
-        # This is currently looking at the durations from point i to all other points
-        i = 0
-        while line:
-            # Get the distances from point i; two spaces is not a typo
-            dist = [float(x) for x in line.split('  ')]
-            # Very rarely, points may not lie within tracts. This is strange, but we'll ignore it
-            print(f'Now processing line {i}..')
-            if i not in pttm:
-                pass
-            # Otherwise, update the tract-pairwise-distance matrix with the pointwise distances
-            else:
-                for j in range(len(dist)):
-                    if j not in pttm:
-                        pass
-                    elif pttm[i] not in tract_distances:
-                        tract_distances[pttm[i]] = {pttm[j]: dist[j]}
-                    elif pttm[j] not in tract_distances[pttm[i]]:
-                        tract_distances[pttm[i]][pttm[j]] = dist[j]
-                    else:
-                        tract_distances[pttm[i]][pttm[j]] += dist[j]
-                    #print(tract_distances)
-            i+=1
-            line = fh.readline()
-
-    # Save tract matrix to file
-    # TODO set filename
-    with open(f'{SAVE_FILE_TO}', 'w') as f:
-        f.write(json.dumps(tract_distances))
 
 '''
 return a mapping of ID to {GEOID, [pf1, ... pf8], pop}
 '''
+
 
 def build_spatial_diversity_dict(tract_dict, geoid_to_id_mapping):
 
@@ -121,21 +69,21 @@ def build_spatial_diversity_dict(tract_dict, geoid_to_id_mapping):
 
                     # Get the population
                     tract_dict[tract_id]['pop'] = tryParseInt(row['pop'])
-                    
+
                     # Get the PF values (used to calculate spatial diversity)
                     pfs = (row['pf1'], row['pf2'],
                            row['pf3'], row['pf4'], row['pf5'],
                            row['pf6'], row['pf4'], row['pf8'])
                     pfs = [tryParse(pf) for pf in pfs]
 
-
                     tract_dict[tract_id]['pfs'] = pfs
 
                     #print (tract_dict[tract_id])
                 else:
                     raise TractNotFoundError
-            
+
     return tract_dict
+
 
 '''
 Given a Graph and an assignment of tracts to districts, calculate the spatial
@@ -147,6 +95,7 @@ Takes in a Partition which has a graph and a partition
 Returns the district's spatial diversity score, a weighted average of the
 individual standard deviations
 '''
+
 
 def calc_spatial_diversity(partition):
     print("calculating spatial diversity")
@@ -176,12 +125,12 @@ def calc_spatial_diversity(partition):
                 spatial_diversity_pops[district_id] = 0
             spatial_diversity_pops[district_id] += pop
             if district_id not in spatial_diversity_sums:
-                spatial_diversity_sums[district_id] = [0,0,0,0,0,0,0,0]
+                spatial_diversity_sums[district_id] = [0, 0, 0, 0, 0, 0, 0, 0]
             # Add element-wise, multiplied by population
             # this is p.q
             for idx, pf in enumerate(spatial_diversity_sums[district_id]):
                 spatial_diversity_sums[district_id][idx] += pfs[idx] * pop
-        #print(spatial_diversity_sums)
+        # print(spatial_diversity_sums)
 
     # loop through again to get the variances
     # this time we're doing pf_i - mean_pfs
@@ -198,25 +147,28 @@ def calc_spatial_diversity(partition):
         # that is, pf_bar = (q.p)/sum(p)
 
         mean_pfs = [x/sum_pops for x in spatial_diversity_sums[district_id]]
-        #print(mean_pfs)
+        # print(mean_pfs)
 
         if pop is None or pfs is None:
             pass
         else:
 
             if district_id not in spatial_diversity_variances:
-                spatial_diversity_variances[district_id] = [0,0,0,0,0,0,0,0]
+                spatial_diversity_variances[district_id] = [
+                    0, 0, 0, 0, 0, 0, 0, 0]
             for idx, pf in enumerate(pfs):
                 # don't ask me why * pop
-                spatial_diversity_variances[district_id][idx] += (pf - mean_pfs[idx])**2 * pop
+                spatial_diversity_variances[district_id][idx] += (
+                    pf - mean_pfs[idx])**2 * pop
 
     # Spatial diversity scores are simply the sqrt of the variances divided by the sum of pops
 
     for district_id in spatial_diversity_variances:
         sum_pop = spatial_diversity_pops[district_id]
-        #print(pop)
-        #print(spatial_diversity_variances[district_id])
-        spatial_diversity_scores[district_id] = [(x/sum_pop) ** 0.5 for x in spatial_diversity_variances[district_id]]
+        # print(pop)
+        # print(spatial_diversity_variances[district_id])
+        spatial_diversity_scores[district_id] = [
+            (x/sum_pop) ** 0.5 for x in spatial_diversity_variances[district_id]]
 
     # We now get each spatial diversity subscore and take the weighted sum to get the final score
     fw = (SPATIAL_DIVERSITY_FACTOR_WEIGHTS)
@@ -226,7 +178,7 @@ def calc_spatial_diversity(partition):
     for district_id in spatial_diversity_scores:
         # Multiply each factor by the factor weight
         score = sum([x*y for x, y in
-            zip(spatial_diversity_scores[district_id], fw)])
+                     zip(spatial_diversity_scores[district_id], fw)])
         # After taking the weighted sum, divide it by the total factor weight
         score /= sum(fw)
         spatial_diversity_final_scores[district_id] = score
@@ -242,7 +194,4 @@ def calc_spatial_diversity(partition):
     return (plan_overall_score, spatial_diversity_final_scores)
 
 
-
 build_spatial_diversity_dict(*get_all_tract_geoids())
-
-
