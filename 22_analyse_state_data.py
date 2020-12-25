cb56f2d40857d2ae5cd350569ea71a59996f688d
Lieu Zheng Hong <lieu@lieuzhenghong.com>
2020-12-25 09:35:54 +0800

    Rename python files to delineate module and main code

--- 22_analyse_state_data.py
+++ 22_analyse_state_data.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 
 
-df = pd.read_csv('./40_docs/data.csv')
+df = pd.read_csv("./40_docs/data.csv")
 print(df.to_latex())
 
 
 def big_positives(row):
-    if (row['pct_variance']) < 0 and row['p-value'] < 0.01:
+    if (row["pct_variance"]) < 0 and row["p-value"] < 0.01:
         return row
     else:
         return None
@@ -15,7 +15,7 @@ def big_positives(row):
 
 def big_negs(row):
     print(row)
-    if (row['pct_variance']) < -5:
+    if (row["pct_variance"]) < -5:
         return row
     return None
 
@@ -24,4 +24,4 @@ big_mags = (df.apply(big_positives, axis=1)).dropna()
 print(big_mags)
 # big_mags.groupby('metric').count()
 
-print(big_mags.groupby('metric').count())
+print(big_mags.groupby("metric").count())
